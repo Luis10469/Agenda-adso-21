c51636922a72999d98af4a94dd9d9243d070a362
xrp-10 <holaxrp@gmail.com>
2026-02-06 11:24:51 -0500

    Update UI

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,1 +1,1 @@
-export default function App() { const fecha = new Date().toLocaleString(); return ( <main> <h1>Hola SENA</h1> <p>Fecha y hora actual: {fecha}</p> </main> ); }git add .
+export default function App() { const fecha = new Date().toLocaleString(); return ( <main> <h1>Hola SENA</h1> <p>Fecha y hora actual: {fecha}</p> </main> ); }
